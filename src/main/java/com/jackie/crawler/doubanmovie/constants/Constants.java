e1dd350c21477b07c687da2a66d363183a22f0ed
DMinerJackie <zjhxp_1990@qq.com>
2017-02-05 20:58:07 +0800

    extract movie and  comments  regular expression into Constants class

--- src/main/java/com/jackie/crawler/doubanmovie/constants/Constants.java
+++ src/main/java/com/jackie/crawler/doubanmovie/constants/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static int maxCycle = 150000;
     public static final String ENCODING = "UTF-8";
     public static final String BLANKSPACE = " ";
+    public static final String MOVIE_REGULAR_EXP = "https://movie.douban.com/subject/\\d{8}";
+    public static final String COMMENT_REGULAR_EXP = "https://movie.douban.com/subject/\\d{8}/comments";
 }
